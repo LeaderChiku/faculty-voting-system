8751a2970f2d236a302e5fb6f73e338267123c8b
LeaderChiku <chauhanaditya339@gmail.com>
2026-03-28 09:47:16 +0530

    fix base url"

--- artifacts/farewell-2026/src/main.tsx
+++ artifacts/farewell-2026/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { setBaseUrl } from "@/lib/custom-fetch";
+import { setBaseUrl } from "@/lib/api-client-react/src/custom-fetch";
 
 setBaseUrl("https://faculty-voting-system.onrender.com");
 createRoot(document.getElementById("root")!).render(<App />);
